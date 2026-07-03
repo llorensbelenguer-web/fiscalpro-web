0cb08e0f47079d04ba32151db853245347ec7de7
llorensbelenguer-web <llorensbelenguer@hotmail.com>
2026-07-03 18:24:46 +0200

    Switch to manual build workflow and simplify code

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,5 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://llorensbelenguer-web.github.io',
-  base: '/fiscalpro-web',
   integrations: [tailwind()],
 });
