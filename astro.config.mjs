959b3e742959c67bfffec924c966e17c6a06600a
llorensbelenguer-web <llorensbelenguer@hotmail.com>
2026-07-04 11:12:08 +0200

    Fix: Final attempt to fix build error with clean config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://llorensbelenguer-web.github.io',
+  base: '/fiscalpro-web',
   integrations: [tailwind()],
+  output: 'static',
 });
